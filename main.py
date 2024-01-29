a68e98cb5dddcdd39d9643c17072234b9da92e78
martex-dev <adedayoniyio@gmail.com>
2024-01-29 03:15:52 +0100

    added fixes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
 
 app = FastAPI()
 
@@ -16,3 +18,7 @@ app.add_middleware(
 from cholesterol_prediction import router as cholesterol_router
 
 app.include_router(cholesterol_router, prefix="/cholesterol", tags=["cholesterol"])
+
+if __name__ == "__main__":
+    # No need to specify host and port when deploying on Render
+    uvicorn.run(app, host="0.0.0.0", port=8000)
